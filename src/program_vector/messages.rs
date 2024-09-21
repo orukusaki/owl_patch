73bff5152c82394c007a284f1df2c6354f606270
Peter Smith <psmith@inviqa.com>
2024-09-21 11:00:48 +0100

    replate critical_section Mutex with spin

--- src/program_vector/messages.rs
+++ src/program_vector/messages.rs
@@ -3,8 +3,7 @@ use core::{
     ffi::{c_char, CStr},
 };
 
-use cortex_m as _;
-use critical_section::Mutex;
+use spin::Mutex;
 
 pub struct Messages {
     message: &'static mut *mut c_char,
@@ -17,15 +16,10 @@ static INSTANCE: Mutex<RefCell<Option<Messages>>> = Mutex::new(RefCell::new(None
 
 impl Messages {
     pub fn init(message: &'static mut *mut c_char) {
-        critical_section::with(|cs| {
-            INSTANCE.replace(
-                cs,
-                Some(Self {
-                    message,
-                    buffer: [0; 64],
-                }),
-            )
-        });
+        INSTANCE.lock().replace(Some(Self {
+            message,
+            buffer: [0; 64],
+        }));
     }
 
     fn debug_message(&mut self, message: &str) {
@@ -37,9 +31,7 @@ impl Messages {
 }
 
 pub fn debug_message(message: &str) {
-    critical_section::with(|cs| {
-        if let Some(messager) = INSTANCE.borrow_ref_mut(cs).as_mut() {
-            messager.debug_message(message)
-        }
-    });
+    if let Some(messager) = INSTANCE.lock().get_mut() {
+        messager.debug_message(message)
+    }
 }
