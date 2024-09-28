bfdfa6081fdf450d8a4b062ded26f6cc1f98f437
Peter Smith <psmith@inviqa.com>
2024-09-28 11:29:13 +0100

    Safer crashes

--- src/lib.rs
+++ src/lib.rs
@@ -3,40 +3,20 @@
 #![feature(array_chunks)]
 #![doc = include_str!("../README.md")]
 
+extern crate alloc;
+
 mod ffi;
 pub mod midi_message;
 pub mod program_vector;
 pub mod sample_buffer;
 
-use ffi::program_vector::ProgramVectorAudioStatus;
-use program_vector::{CONFIGURATION_ERROR_STATUS, PROGRAM_VECTOR};
+use alloc::format;
+use program_vector::error;
 
-use core::{
-    ffi::c_char,
-    panic::PanicInfo,
-    sync::atomic::{compiler_fence, Ordering},
-};
+use core::panic::PanicInfo;
 #[panic_handler]
 unsafe fn panic_handler(info: &PanicInfo) -> ! {
-    let pv = unsafe { PROGRAM_VECTOR.assume_init_mut() };
-
-    let message = info.message().as_str().unwrap_unchecked();
-    let mut buff = [0u8; 64];
-    let len = message.len().min(64 - 1);
-    buff[..len].copy_from_slice(&message.as_bytes()[..len]);
-
-    pv.error = CONFIGURATION_ERROR_STATUS;
-    pv.message = buff.as_mut_ptr() as *mut c_char;
-
-    if let Some(program_status) = pv.programStatus {
-        // This function never returns
-        unsafe {
-            program_status(ProgramVectorAudioStatus::AUDIO_ERROR_STATUS);
-        }
-    }
-    loop {
-        compiler_fence(Ordering::SeqCst);
-    }
+    error(&format!("{}", info.message()))
 }
 
 #[no_mangle]
--- src/program_vector/messages.rs
+++ src/program_vector/messages.rs
@@ -1,9 +1,20 @@
-use core::{cell::RefCell, ffi::c_char};
+use ::core::option::Option;
+use core::{
+    cell::RefCell,
+    ffi::c_char,
+    sync::atomic::{compiler_fence, Ordering},
+};
 
 use spin::Mutex;
 
+use crate::ffi::program_vector::ProgramVectorAudioStatus;
+
+use super::CONFIGURATION_ERROR_STATUS;
+
 pub struct Messages<'a> {
     message: &'a mut *mut c_char,
+    error: &'a mut i8,
+    program_status: Option<unsafe extern "C" fn(status: ProgramVectorAudioStatus)>,
     buffer: [u8; 64],
 }
 
@@ -12,11 +23,29 @@ unsafe impl<'a> Send for Messages<'a> {}
 static INSTANCE: Mutex<RefCell<Option<Messages>>> = Mutex::new(RefCell::new(None));
 
 impl Messages<'static> {
-    pub fn init(message: &'static mut *mut c_char) {
-        INSTANCE.lock().replace(Some(Self {
+    pub fn init(
+        message: &'static mut *mut c_char,
+        error: &'static mut i8,
+        program_status: Option<unsafe extern "C" fn(status: ProgramVectorAudioStatus)>,
+    ) {
+        INSTANCE
+            .lock()
+            .replace(Some(Self::new(message, error, program_status)));
+    }
+}
+
+impl<'a> Messages<'a> {
+    pub fn new(
+        message: &'a mut *mut c_char,
+        error: &'a mut i8,
+        program_status: Option<unsafe extern "C" fn(status: ProgramVectorAudioStatus)>,
+    ) -> Self {
+        Self {
             message,
-            buffer: [0; 64],
-        }));
+            error,
+            program_status,
+            buffer: [0u8; 64],
+        }
     }
 
     fn debug_message(&mut self, message: &str) {
@@ -25,10 +54,32 @@ impl Messages<'static> {
         self.buffer[len] = 0;
         *self.message = self.buffer.as_mut_ptr() as *mut c_char
     }
+
+    fn error(&mut self, message: &str) {
+        self.debug_message(message);
+        *self.error = CONFIGURATION_ERROR_STATUS;
+
+        if let Some(program_status) = self.program_status {
+            // This function never returns
+            unsafe {
+                program_status(ProgramVectorAudioStatus::AUDIO_ERROR_STATUS);
+            }
+        }
+    }
 }
 
 pub fn debug_message(message: &str) {
-    if let Some(messager) = INSTANCE.lock().get_mut() {
-        messager.debug_message(message)
+    if let Some(instance) = INSTANCE.lock().get_mut() {
+        instance.debug_message(message)
+    }
+}
+
+pub fn error(message: &str) -> ! {
+    if let Some(instance) = INSTANCE.lock().get_mut() {
+        instance.error(message);
+    } // else { ¯\(ツ)/¯ }
+
+    loop {
+        compiler_fence(Ordering::SeqCst);
     }
 }
