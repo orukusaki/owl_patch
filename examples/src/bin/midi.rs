46c8a74556e1e21805a8174c2814ae9a9844f07c
Peter Smith <psmith@inviqa.com>
2024-11-03 11:15:00 +0000

    Use vpo conversions in midi example

--- examples/src/bin/midi.rs
+++ examples/src/bin/midi.rs
@@ -4,12 +4,12 @@ extern crate alloc;
 
 use alloc::sync::Arc;
 use core::sync::atomic::{AtomicU32, Ordering};
-use num_traits::Float;
 use owl_patch::{
     midi_message::MidiMessage,
     patch,
     program_vector::{heap_bytes_used, ProgramVector},
     sample_buffer::{Buffer, ConvertTo},
+    volts_per_octave::{Frequency, Note},
 };
 
 #[patch("Midi Example")]
@@ -27,9 +27,9 @@ fn run(mut pv: ProgramVector) -> ! {
     let midi = pv.midi();
     midi.on_receive(move |message: MidiMessage| {
         if message.is_note_on() {
-            let note = message.note();
-            let hz = 440.0 * 2.0f32.powf((note as f32 - 69.0) / 12.0);
-            inc.store((mul * hz).to_bits(), Ordering::Relaxed);
+            let note = Note(message.note());
+            let hz = Frequency::from(note);
+            inc.store((mul * hz.0).to_bits(), Ordering::Relaxed);
         }
         midi.send(message);
     });
